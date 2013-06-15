1e2f577a845f9acb70dbb3baa2642bada4f9e040
vergeman <alan.verga@gmail.com>
2013-06-15 11:29:53 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fosterdog::Application.config.secret_token = 'fbc294552e314df69d85cc34c0363d45c212fe21586f88312bf1a6b6765a71d9ec1ff4b421a6e8402cc9c647305db65857b6a3e3d51ab11d3de37d0d28390de5'

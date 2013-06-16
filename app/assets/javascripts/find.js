95b4a19a858178143940c78c6a9356f984b6c816
vergeman <alan.verga@gmail.com>
2013-06-16 02:05:38 -0400

    dogbrowser

---
+++ app/assets/javascripts/find.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function() {
+
+    $(".panel-attributes :checkbox").click(function() {
+	console.log($(this).val());
+	//then we hide all
+
+	//reveal only those that match if checked
+    });
+
+
+});
+
